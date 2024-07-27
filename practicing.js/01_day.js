7f3b322f88a9a9247ceb02bf6d955ca8f6a0c2b4
Rohit-editor <inspirecreate984@gmail.com>
2024-07-27 07:35:57 +0000

    here is 2nd file

---
+++ practicing.js/01_day.js
@@ -0,0 +1,12 @@
+switch (1) {
+    case 1:
+        console.log("here is 1");
+        break;
+    case 2:
+    console.log("here is 2");
+    break;
+        
+
+    default: console.log("here is default case");
+        break;
+}
